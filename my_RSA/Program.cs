408719d19af5c9b0fd82a688acf14026d62db6d8
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2015-11-24 01:50:29 +0100

    using Extended Euclidean algorithm to calculate d

--- my_RSA/Program.cs
+++ my_RSA/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace my_RSA
 {
@@ -16,22 +17,23 @@ namespace my_RSA
         }
         class RSA
         {
-            private int p = 5;//19;
-            private int q = 11;//13;
-            private int n;
-            private int e;
-            private int d;
-            private List<int> primaryNumbers = new List<int>();
-            private List<int> possible_d = new List<int>();
-            private int[] publickey = new int[2];
-            private int[] privatekey = new int[2];
+            private BigInteger p = 61;
+            private BigInteger q = 73;
+            private BigInteger n;
+            private BigInteger e;
+            private BigInteger d;
+            private BigInteger phi;
+            private List<BigInteger> primaryNumbers = new List<BigInteger>();
+            private List<BigInteger> possible_d = new List<BigInteger>();
+            private BigInteger[] publickey = new BigInteger[2];
+            private BigInteger[] privatekey = new BigInteger[2];
             public RSA() {
                 generateKey();
             }
             private void generateKey()
             {
                 n = p * q;
-                int phi = (p - 1) * (q - 1);
+                phi = (p - 1) * (q - 1);
                 for (int i = 0; i < phi; i++)
                 {
                     if (IsPrime(i) && (n % i) != 0)
@@ -39,70 +41,61 @@ namespace my_RSA
                 }
                 Random r = new Random();
                 e = primaryNumbers[r.Next(0,primaryNumbers.Count - 1)];
-                //find_d(phi);
-                //d = possible_d[r.Next(0, possible_d.Count - 1)];
-                do
-                {
-                    d = ExtendedEuclidean(e % phi, phi).u1;
-                } while (d > 0);
+                find_d();
                 Console.WriteLine("e: " + e + "\nd: " + d + "\nn: " + n);
                 publickey[0] = e;
                 publickey[1] = n;
-                privatekey[0] = d;
+                privatekey[0] = (int)d;
                 privatekey[1] = n;
             }
-            private ExtendedEuclideanResult ExtendedEuclidean(int a, int b)
+            //Extended Euclidean algorithm
+            private void find_d()
             {
-                int u1 = 1;
-                int u3 = a;
-                int v1 = 0;
-                int v3 = b;
 
-                while (v3 > 0)
+                long[] u;
+                long[] v;
+                long q, temp1, temp2, temp3;
+
+                u = new long[] { 0, 0, 0 };
+                v = new long[] { 0, 0, 0 };
+
+                u[0] = 1; u[1] = 0; u[2] = (long)phi;
+                v[0] = 0; v[1] = 1; v[2] = (long)e;
+
+                while (v[2] != 0)
                 {
-                    int q0 = u3 / v3;
-                    int q1 = u3 % v3;
-
-                    int tmp = v1 * q0;
-                    int tn = u1 - tmp;
-                    u1 = v1;
-                    v1 = tn;
-
-                    u3 = v3;
-                    v3 = q1;
+                    q = (long)Math.Floor((decimal)u[2] / v[2]);
+                    temp1 = u[0] - q * v[0];
+                    temp2 = u[1] - q * v[1];
+                    temp3 = u[2] - q * v[2];
+                    u[0] = v[0];
+                    u[1] = v[1];
+                    u[2] = v[2];
+                    v[0] = temp1;
+                    v[1] = temp2;
+                    v[2] = temp3;
                 }
+                if (u[1] < 0) d = (u[1] + phi);
+                else d = (u[1]);
 
-                int tmp2 = u1 * (a);
-                tmp2 = u3 - (tmp2);
-                int res = tmp2 / (b);
-
-                ExtendedEuclideanResult result = new ExtendedEuclideanResult()
-                {
-                    u1 = u1,
-                    u2 = res,
-                    gcd = u3
-                };
-
-                return result;
-            }
-            private void find_d(int phi)
-            {
-                for (int i = 0; i < n; i++)
-                {
-                    d = e * i;
-                    if (d % phi == 1)
-                        possible_d.Add(d);
-                }
             }
             public uint encrypt(uint m)
             {
-                uint c = Convert.ToUInt32(Math.Pow(m, publickey[0]) % publickey[1]);
-                return c;
+                BigInteger bint = BigInteger.ModPow(m, publickey[0],publickey[1]);
+                return Convert.ToUInt32(bint.ToString());
             }
             public uint decrpyt(uint c)
             {
-                UInt64 m = Convert.ToUInt64((Math.Pow(c, privatekey[0])) % privatekey[1]);
-                return Convert.ToUInt32(m);
+                BigInteger bint = BigInteger.ModPow(c, privatekey[0], privatekey[1]);
+                return Convert.ToUInt32(bint.ToString());
+            }
+            public BigInteger[] getPublicKey()
+            {
+                return publickey;
+            }
+            public BigInteger[] getPrivateKey()
+            {
+                return privatekey;
             }
             private bool IsPrime(int candidate)
             {
@@ -125,10 +118,7 @@ namespace my_RSA
         static void Main(string[] args)
         {
             RSA rsa = new RSA();
-            //string input = "a";
-            int input = 2;
-            //char[] charinput = input.ToCharArray();
-           // uint ASCII = Convert.ToUInt32(charinput[0]);
+            int input = 98;
             uint ASCII = Convert.ToUInt32((uint)input);
             Console.WriteLine("Przed: " + ASCII);
             uint encrypted = rsa.encrypt(ASCII);
